d0eddcf1f94571c0ac90518d47c53ea57dfa3eed
kevin harris <kcharris77@gmail.com>
2025-11-30 21:22:59 -0800

    Add d1p2 solution

--- Day1/answer2.py
+++ Day1/answer2.py
@@ -1,1 +1,15 @@
 f = open("Day1\data.txt")
+res = 0
+curr = 50
+for line in f.readlines():
+    direction = 1
+    letter, num = line[0], int(line[1:])
+    if letter == "L":
+        direction = -1
+
+    for i in range(num):
+        curr = (curr + direction) % 100
+        if curr == 0:
+            res += 1
+
+print(res)
