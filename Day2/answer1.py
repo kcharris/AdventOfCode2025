418ff4aafcb510267aab6c4de53db960964069d0
kevin harris <kcharris77@gmail.com>
2025-12-01 21:11:29 -0800

    Add d2p1 solution

--- Day2/answer1.py
+++ Day2/answer1.py
@@ -1,1 +1,16 @@
 f = open("Day2\data.txt")
+
+data = f.readline().split(",")
+
+res = 0
+for s in data:
+    a, b = [int(x) for x in s.split("-")]
+
+    for i in range(a, b+1):
+        sn = str(i)
+        if len(sn) & 1 == 1:
+            continue
+        if sn[:len(sn)//2] == sn[len(sn)//2:]:
+            res += i
+
+print(res)
