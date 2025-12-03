2067ee9d62d5619c8c46c96dc53618681c06e67a
kevin harris <kcharris77@gmail.com>
2025-12-02 21:39:22 -0800

    Add d3 p1 and p2 solution

--- Day3/answer1.py
+++ Day3/answer1.py
@@ -1,1 +1,17 @@
 f = open("Day3\data.txt")
+res = 0
+for line in f.readlines():
+    arr = [int(c) for c in line.strip()]
+    m1 = max(arr[:len(arr)-1])
+    r = 0
+    while arr[r] != m1:
+        r += 1
+    r += 1
+    m2 = 0
+    while r < len(arr):
+        m2 = max(m2, arr[r])
+        r += 1
+    
+    res += m1*10 + m2
+
+print(res)
--- Day3/answer2.py
+++ Day3/answer2.py
@@ -1,1 +1,25 @@
 f = open("Day3\data.txt")
+def getIndexOfMax(l, r):
+    m_found = -1
+    idx = -1
+    for i in range(l, r):
+        if m_found < arr[i]:
+            m_found = arr[i]
+            idx = i
+    return idx
+
+res = 0
+for line in f.readlines():
+    arr = [int(c) for c in line.strip()]
+    max_arr = [-1]
+    for i in range(0,12):
+        l = max_arr[-1] + 1
+        r = len(arr) - (11-i)
+        j = getIndexOfMax(l, r)
+        max_arr.append(j)
+    pwr = 0
+    while pwr < 12:
+        res += 10**pwr * arr[max_arr.pop()]
+        pwr += 1
+
+print(res)
