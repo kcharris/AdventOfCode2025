945265c5dcb14dcc1b9e72ef1712b6177dcc5ff1
kevin harris <kcharris77@gmail.com>
2025-12-03 22:36:38 -0800

    Add day4 p1 and p2 solutions

--- Day4/answer1.py
+++ Day4/answer1.py
@@ -1,1 +1,26 @@
 f = open("Day4\data.txt")
+
+grid = []
+def isValid(r, c):
+    if r < len(grid) and r >= 0 and c < len(grid[0]) and c >= 0:
+        return True
+    return False
+
+def getRollCount(r, c):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if not (i == 0 and j == 0):
+                if isValid(r+i, c+j) and grid[r+i][c+j] == "@":
+                    count += 1
+    return count
+
+for line in f.readlines():
+    grid.append(line.strip())
+
+res = 0
+for r in range(len(grid)):
+    for c in range(len(grid)):
+        if grid[r][c] == "@" and getRollCount(r, c) < 4:
+            res += 1
+print(res)
--- Day4/answer2.py
+++ Day4/answer2.py
@@ -1,1 +1,50 @@
-f = open("Day4\data.txt")
+from sortedcontainers import SortedSet
+f = open("Day4\data1.txt")
+
+removed = set()
+roll_d = {}
+ss = SortedSet()
+grid = []
+def isValid(r, c):
+    if r < len(grid) and r >= 0 and c < len(grid[0]) and c >= 0:
+        return True
+    return False
+
+def getRollCount(r, c):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if not (i == 0 and j == 0):
+                if isValid(r+i, c+j) and grid[r+i][c+j] == "@":
+                    count += 1
+    return count
+
+def removeRollAndUpdateCounts(r,c):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if not (i == 0 and j == 0):
+                r1, c1 = r+i, c+j
+                if isValid(r1, c1) and grid[r1][c1] == "@" and (r1, c1) not in removed:
+                    prev_count = roll_d[(r1, c1)]
+                    roll_d[(r1, c1)] -= 1
+                    ss.remove((-prev_count, r1, c1))
+                    ss.add((-roll_d[(r1, c1)], r1, c1))
+
+for line in f.readlines():
+    grid.append(line.strip())
+
+res = 0
+for r in range(len(grid)):
+    for c in range(len(grid)):
+        if grid[r][c] == "@":
+            roll_count = getRollCount(r,c)
+            roll_d[(r, c)] = roll_count
+            ss.add((-roll_count, r, c))
+            
+while -ss[-1][0] < 4:
+    res += 1
+    _, r, c = ss.pop()
+    removed.add((r, c))
+    removeRollAndUpdateCounts(r, c)
+    
+print(res)
