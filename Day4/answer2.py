eb65b486a18101a7725fc5ecdb776d5c639dc56c
kevin harris <kcharris77@gmail.com>
2025-12-03 23:30:13 -0800

    Remove unnecessary set and update fn name

--- Day4/answer2.py
+++ Day4/answer2.py
@@ -3,7 +3,6 @@ import time
 time_start = time.time()
 f = open("Day4\data.txt")
 
-removed = set()
 roll_d = {}
 ss = SortedSet()
 grid = []
@@ -21,12 +20,12 @@ def getRollCount(r, c):
                     count += 1
     return count
 
-def removeRollAndUpdateCounts(r,c):
+def updateCounts(r,c):
     for i in range(-1, 2):
         for j in range(-1, 2):
             if not (i == 0 and j == 0):
                 r1, c1 = r+i, c+j
-                if isValid(r1, c1) and grid[r1][c1] == "@" and (r1, c1) not in removed:
+                if isValid(r1, c1) and grid[r1][c1] == "@" and (r1, c1) in roll_d:
                     prev_count = roll_d[(r1, c1)]
                     roll_d[(r1, c1)] -= 1
                     ss.remove((-prev_count, r1, c1))
@@ -46,8 +45,8 @@ for r in range(len(grid)):
 while -ss[-1][0] < 4:
     res += 1
     _, r, c = ss.pop()
-    removed.add((r, c))
-    removeRollAndUpdateCounts(r, c)
+    del roll_d[(r, c)]
+    updateCounts(r, c)
 time_end = time.time()
 print(time_end - time_start)
 
