a0b09929720f6ff8fc26b0184b5c19cfe80029be
kevin harris <kcharris77@gmail.com>
2025-12-05 22:09:47 -0800

    Add day6 p1 and p2 solutions

--- Day6/answer1.py
+++ Day6/answer1.py
@@ -1,1 +1,20 @@
-f = open("Day6\data.txt")
+f = open("Day6\data1.txt")
+
+arr = []
+for line in f.readlines():
+    arr.append(line.strip().split())
+    # print(arr)
+    if arr[-1][0] not in ["*", "+"]:
+        arr[-1] = list(map(int, arr[-1]))
+
+res_arr = [arr[0][i] for i in range(len(arr[0]))]
+for r in range(1, len(arr)-1):
+    for c in range(len(arr[r])):
+        if arr[-1][c] == "+":
+            res_arr[c] += arr[r][c]
+        else:
+            res_arr[c] *= arr[r][c]
+
+print(sum(res_arr))
+
+
--- Day6/answer2.py
+++ Day6/answer2.py
@@ -1,1 +1,33 @@
+from functools import reduce
 f = open("Day6\data.txt")
+
+arr_in = []
+for line in f.readlines():
+    arr_in.append(line.strip("\n"))
+
+def do_math(start, end):
+    res = [[0 for _ in range(end-start)] for _ in range(len(arr_in))]
+    for r in range(len(arr_in)-1):
+        for c in range(start, end):
+            res_c = c-start
+            if arr_in[r][c] != " ":
+                res[r+1][res_c] = res[r][res_c] * 10 + int(arr_in[r][c])
+            else:
+                res[r+1][res_c] = res[r][res_c]
+    if arr_in[-1][start] == "+":
+        return sum(res[-1])
+    return reduce(lambda ans, x: ans * x, res[-1], 1)
+
+math_lens = []
+start = 0
+for i in range(1, len(arr_in[0])):
+    if arr_in[-1][i] in ["+", "*"]:
+        math_lens.append([start, i-1])
+        start = i
+math_lens.append([start, len(arr_in[0])])
+
+res = 0
+for start, end in math_lens:
+    res += do_math(start, end)
+
+print(res)
