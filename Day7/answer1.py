54bc33a3abcaaf04a8586bc8b2b2eb3f28b5b5df
kevin harris <kcharris77@gmail.com>
2025-12-06 21:45:50 -0800

    Add Day7 p1 and p2 solution

--- Day7/answer1.py
+++ Day7/answer1.py
@@ -1,1 +1,31 @@
-f = open("Day7\data.txt")
+f = open("Day7\data1.txt")
+
+g = []
+for line in f.readlines():
+    g.append([c for c in line.strip()])
+
+def getStart():
+    for r in range(len(g)):
+        for c in range(len(g[0])):
+            if g[r][c] == "S":
+                return (r, c)
+start = getStart()
+
+def isValid(r, c):
+    if r < len(g) and r >= 0 and c < len(g[0]) and c >= 0:
+        return True
+    return False
+
+visited = set()
+def fireLaser(r, c):
+    if not isValid(r, c) or (r,c) in visited:
+        return 0
+    visited.add((r, c))
+
+    if g[r][c] == "^":
+        return 1 + fireLaser(r+1, c-1) + fireLaser(r+1, c+1)
+    return fireLaser(r+1, c)
+
+print(fireLaser(start[0], start[1]))
+
+
--- Day7/answer2.py
+++ Day7/answer2.py
@@ -1,1 +1,31 @@
-f = open("Day7\data.txt")
+from functools import cache
+f = open("Day7\data1.txt")
+
+g = []
+for line in f.readlines():
+    g.append([c for c in line.strip()])
+
+def getStart():
+    for r in range(len(g)):
+        for c in range(len(g[0])):
+            if g[r][c] == "S":
+                return (r, c)
+start = getStart()
+
+def isValid(r, c):
+    if r < len(g) and r >= 0 and c < len(g[0]) and c >= 0:
+        return True
+    return False
+
+@cache
+def fireLaser(r, c):
+    if r == len(g):
+        return 1
+    if not isValid(r, c):
+        return 0
+
+    if g[r][c] == "^":
+        return fireLaser(r+1, c-1) + fireLaser(r+1, c+1)
+    return fireLaser(r+1, c)
+
+print(fireLaser(start[0], start[1]))
