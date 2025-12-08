6c1435bffb3222ac8a0ae745984ed4a8c5af7817
kevin harris <kcharris77@gmail.com>
2025-12-08 00:23:36 -0800

    Add day 8 p1 and p2 solutions

--- Day8/answer1.py
+++ Day8/answer1.py
@@ -1,1 +1,68 @@
+import math
+from queue import PriorityQueue
+from pprint import pprint
 f = open("Day8\data.txt")
+
+g = {}
+points = []
+pq = PriorityQueue()
+
+def getDistance(p1, p2):
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+
+def sameCircuit(p1i, p2i):
+    visited = {p1i}
+    nodes = [p1i]
+    while len(nodes) > 0:
+        node = nodes.pop()
+        if node == p2i:
+            return True
+        for next_node in g[node]:
+            if next_node not in visited:
+                visited.add(next_node)
+                nodes.append(next_node)
+    return False
+
+for line in f.readlines():
+    a, b, c = map(int, line.strip().split(","))
+    points.append((a, b, c))
+
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        tup = (getDistance(points[i], points[j]), i, j)
+        pq.put(tup)
+
+count = 0
+while pq.qsize() > 0 and count < 1000:
+    dist, p1_i, p2_i = pq.get()
+    if (p1_i not in g or p2_i not in g) or not sameCircuit(p1_i, p2_i):
+        g.setdefault(p1_i, set()).add(p2_i)
+        g.setdefault(p2_i, set()).add(p1_i)
+    count += 1
+
+res = 1
+visited = set()
+pq = PriorityQueue()
+for i in range(3):
+    pq.put(-1)
+for i in g:
+    count = 0
+    if i not in visited:
+        nodes = [i]
+        visited.add(i)
+        while len(nodes):
+            node = nodes.pop()
+            count += 1
+            for new_node in g[node]:
+                if new_node not in visited:
+                    visited.add(new_node)
+                    nodes.append(new_node)
+    if count > 0:
+        low = pq.get()
+        pq.put(max(low, count))
+
+res = 1
+while pq.qsize() > 0:
+    res *= pq.get()
+print(res)
+            
--- Day8/answer2.py
+++ Day8/answer2.py
@@ -1,1 +1,41 @@
+import math
+from queue import PriorityQueue
+from pprint import pprint
 f = open("Day8\data.txt")
+
+g = {}
+points = []
+pq = PriorityQueue()
+
+def getDistance(p1, p2):
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+
+for line in f.readlines():
+    a, b, c = map(int, line.strip().split(","))
+    points.append((a, b, c))
+
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        tup = (getDistance(points[i], points[j]), i, j)
+        pq.put(tup)
+
+while pq.qsize() > 0:
+    dist, p1_i, p2_i = pq.get()
+    if (p1_i not in g or p2_i not in g) or p1_i not in g[p2_i]:
+        g.setdefault(p1_i, set()).add(p2_i)
+        g.setdefault(p2_i, set()).add(p1_i)
+        if len(g[p1_i]) > len(g[p2_i]):
+            g[p1_i].update(g[p2_i])
+            g[p2_i] = g[p1_i]
+        else:
+            g[p2_i].update(g[p1_i])
+            g[p1_i] = g[p2_i]
+        for k in g[p1_i]:
+            g[k] = g[p1_i]
+        
+    if len(g[p1_i]) == len(points):
+        print(points[p1_i], points[p2_i])
+        print(points[p1_i][0] * points[p2_i][0])
+        break
+
+print("end")
