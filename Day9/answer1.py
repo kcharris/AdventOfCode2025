18a56958bc48a0cbe2e57a2180e7db984c6f32aa
kevin harris <kcharris77@gmail.com>
2025-12-08 23:25:46 -0800

    Add day 9 pt1 solution

--- Day9/answer1.py
+++ Day9/answer1.py
@@ -1,1 +1,17 @@
 f = open("Day9\data.txt")
+
+points = []
+for line in f.readlines():
+    points.append(list(map(int, line.strip().split(","))))
+
+def getArea(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+
+    return abs(x1-x2+1) * abs(y1-y2+1)
+
+res = 0
+for p1 in points:
+    for p2 in points:
+        res = max(res, getArea(p1, p2))
+print(res)
