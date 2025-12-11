2d525f34b86bbe7acc7ab2759b7a2cc03912fc9a
kevin harris <kcharris77@gmail.com>
2025-12-10 22:06:02 -0800

    Add day 11 pt1 solution

--- Day11/answer1.py
+++ Day11/answer1.py
@@ -1,1 +1,43 @@
-f = open("Day11\data.txt")
+from functools import cache
+def parseLine(line):
+    line = line.strip()
+    i = 0
+    while line[i] != ":":
+        i += 1
+    node = line[:i]
+    i += 1
+    next_nodes = line[i:].split()
+    return node, next_nodes
+
+def solve(f):
+    graph = {}
+    for line in f.readlines():
+        node, next_nodes = parseLine(line)
+        graph.setdefault(node, next_nodes)
+
+    visited = set()
+    @cache
+    def dfsPathCount(node):
+        if node == "out":
+            return 1
+        if node not in graph:
+            return 0
+        
+        res = 0
+        for n in graph[node]:
+            if n not in visited:
+                visited.add(n)
+                res += dfsPathCount(n)
+                visited.remove(n)
+        return res
+    
+    return dfsPathCount("you")
+
+f = open("Day11\data1.txt")
+data1_answer = 5
+answer1 = solve(f)
+print(f"Test answer is {answer1}. Test expected is {data1_answer}")
+
+if answer1 == data1_answer:
+    f = open("Day11\data.txt")
+    print(solve(f))
